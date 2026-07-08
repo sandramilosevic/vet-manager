8cadad7b03995fb798cef0918ca73ca6898d4293
Sandra Milosevic <smilosevic892@gmail.com>
2026-07-08 11:40:28 +0200

    custom UserAdmin extending Django's built-in auth UserAdmin, which uses ReadOnlyPasswordHashField

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Invitation
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """
+    Extends Django's built-in UserAdmin so the password field is shown
+    as a read-only hash + "change password" link
+    """
+
+    # Add our custom fields (clinic, role) to the existing fieldsets
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Clinic info", {"fields": ("clinic", "role")}),
+    )
+    list_display = ("username", "email", "clinic", "role", "is_staff")
+    list_filter = BaseUserAdmin.list_filter + ("clinic", "role")
+
+
 admin.site.register(Invitation)
