da99a59797bb8c21c988c10f6966f496a259b78a
Sandra Milosevic <smilosevic892@gmail.com>
2026-07-09 23:59:13 +0200

    audit fields for users - created and updated at

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -32,6 +32,9 @@ class User(AbstractUser):
 
     email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLES, default="VET")
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
